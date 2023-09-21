fbbbdfd3c322c49b086aeb03acbc1abc6d801793
lokeshbn <lokeshnarasimha353@gmail.com>
2023-09-21 19:29:46 +0530

    updated

--- Lambdafunction/Example.py
+++ Lambdafunction/Example.py
@@ -1,10 +1,25 @@
-import logging
+import boto3
 
-LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.INFO)
+# Initialize the DynamoDB client
+dynamodb = boto3.client('dynamodb', region_name='ap-south-1')
 
-def handler(event, context):
-    LOGGER.info(f'Event Object: {event}')
-    LOGGER.info(f'Context Object: {context}')
-    event['DemoChannel'] = 'CloudQuickPOCs'
-    return event
+# Define the table name
+table_name = 'GameScorese'
+
+# Define the item (record) you want to add
+new_item = {
+    'PrimaryKeyName': {'S': 'Primary_Key_Value'},
+    'AttributeName1': {'S': 'Value1'},
+    'AttributeName2': {'N': '123'},
+    # Add more attributes as needed
+}
+
+# Add the item to the DynamoDB table
+try:
+    response = dynamodb.put_item(
+        TableName=GameScores,
+        Item=new_item
+    )
+    print("Item added successfully!")
+except Exception as e:
+    print(f"Error adding item: {str(e)}")
