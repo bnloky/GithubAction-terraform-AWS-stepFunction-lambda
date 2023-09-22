136a9a9f78387515cbbd36eba71530a317606b60
lokeshbn <lokeshnarasimha353@gmail.com>
2023-09-22 18:08:15 +0530

    curly quotes are removed from stepfile

--- Stepfunction/Stepfunction.tf
+++ Stepfunction/Stepfunction.tf
@@ -3,19 +3,19 @@ variable "pythonfunctionapparn" {
 }
 
 ## Aws step function role 
-resource “aws_iam_role” "step_function_role” {
-name = "my-stepfunction-role™
+resource "aws_iam_role" "step_function_role" {
+name = "my-stepfunction-role"
 assume_role_policy = <<-EOF
  {
    "Version": 2012-10-17",
    "Statement": [
       {
-        “Action”: "sts:AssumeRole”,
-        “principal”: {
-          "Service": “states.amazonaws.com"
+        "Action": "sts:AssumeRole",
+        "principal": {
+          "Service": "states.amazonaws.com"
         },
-        “Effect": "Allow",
-        "Sid": "StepFunctionAssumeRole”
+        "Effect": "Allow",
+        "Sid": "StepFunctionAssumeRole"
       }
    ]
   }
@@ -23,8 +23,8 @@ assume_role_policy = <<-EOF
 }
 
 ## Aws Step function role-policy
-resource “aws_iam_role_policy” "step_function_policy” {
-name    = "My-stepfunctionrole-policy”
+resource "aws_iam_role_policy" "step_function_policy" {
+name    = "My-stepfunctionrole-policy"
 role    = aws_iam_role.step_function_role.id
 
 policy = <<-EOF
@@ -36,7 +36,7 @@ policy = <<-EOF
           "lambda:InvokeFunction"
        ],
        "Effect": "Allow",
-       “Resource”: “${var.pythonfunctionapparn}”
+       "Resource": "${var.pythonfunctionapparn}"
       }
     ]
   }
@@ -44,15 +44,15 @@ policy = <<-EOF
 }
 
 ##AWS State function - State machine
-resource "aws_sfn_state machine” "My_state_machine” {
+resource "aws_sfn_state machine" "My_state_machine" {
 name     =  "My-demo-stepfunction"
 role_arn = aws_iam_role.step_function_role.arn
 
 definition = <<EOF
 {
-  “Comment”: "Invoke AWS Lambda from AWS Step Functions with Terraform",
-  “StartAt": “ExampleLambdaFunctionApp”,
-  “States": {
+  "Comment": "Invoke AWS Lambda from AWS Step Functions with Terraform",
+  "StartAt": "ExampleLambdaFunctionApp",
+  "States": {
     "ExampleLambdaFunctionApp": {
         "Type": "Task",
         "Resource": "${var.pythonfunctionapparn}",
